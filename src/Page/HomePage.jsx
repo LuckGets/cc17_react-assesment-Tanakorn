059b1283eeede3e01074b82b616ca020b31edbf6
Tanakorn <kasides12@gmail.com>
2024-05-08 11:22:45 +0700

    set navigate

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { Box, Typography, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import TaskItem from "../Components/TaskItem";
 
 function HomePage() {
+  const navigate = useNavigate();
+  // set Task state
+  const [task, setTask] = useState([]);
+  const [inputTask, setInputTask] = useState("");
+
+  // Handle Change Input
+  const handleChangeInput = (e) => setInputTask(e.target.value);
+
+  const handleLogOut = (e) => {
+    e.preventDefault();
+    const logoutResult = confirm("Are you sure you want to logout?");
+    if (logoutResult) {
+      navigate("/login");
+    }
+    return;
+  };
+
+  const handleAdd = () => {
+    const newTask = [...task];
+    newTask.push(inputTask);
+    setTask(newTask);
+  };
   return (
-    <div>HomePage</div>
-  )
+    <Box
+      border="4px solid red"
+      sx={{ width: "90vw", height: 1, paddingInline: "40px" }}
+    >
+      <Box>
+        <Typography variant="h1">My Todo</Typography>
+        <TextField
+          sx={{ width: "100%" }}
+          onChange={handleChangeInput}
+          value={inputTask}
+          label="new task"
+        ></TextField>
+      </Box>
+      <TaskItem />
+      <Box sx={{ width: "100%", height: 1 }}>
+        <Button
+          sx={{ width: "100%", paddingBlock: "20px", borderRadius: "20px" }}
+          variant="contained"
+          onClick={handleLogOut}
+        >
+          LOG OUT
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/Page/LoginPage.jsx
+++ src/Page/LoginPage.jsx
@@ -1,11 +1,31 @@
-// import { Box } from '@mui/material'
+import { Box, Typography, TextField, Button } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function LoginPage() {
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    if (true){
+    navigate("/")
+    }
+    else return
+  };
+
   return (
-    // <Box>
-    <h1>Login Page</h1>
-    // </Box>
+    <Box sx={{paddingInline:"50px"}} justifyContent="center" width="90vw" height="80vh" display="flex" flexDirection="column" border="4px solid red" >
+      <Box sx={{}}>
+        <Typography variant='h1'>Welcome</Typography>
+      </Box>
+      <Box sx={{marginBottom: "100px"}} display="flex" flexDirection="column" gap="10px">
+        <TextField label='E-mail or Mobile' variant='outlined'></TextField>
+        <TextField label='password' variant='outlined'></TextField>
+      </Box>
+      <Box display="flex" justifyContent='center'>
+        <Button type="button" onClick={handleLogin} variant="contained" sx={{width: 0.8, paddingBlock: "20px", borderRadius:"25px", fontSize: "1.1rem"}}>LOG IN</Button>
+      </Box>
+    </Box>
   )
 }
 
